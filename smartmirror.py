3b7c88c02e638cbcfbb201d2b4b4c22ca1005dfd
Alizamin Jafarli <jafarli_alizamin@yahoo.com>
2018-12-08 15:49:38 +0100

    Update smartmirror.py

--- smartmirror.py
+++ smartmirror.py
@@ -17,18 +17,18 @@ from contextlib import contextmanager
 LOCALE_LOCK = threading.Lock()
 
 ui_locale = '' # e.g. 'fr_FR' fro French, '' as default
-time_format = 12 # 12 or 24
-date_format = "%b %d, %Y" # check python doc for strftime() for options
-news_country_code = 'us'
-weather_api_token = '<TOKEN>' # create account at https://darksky.net/dev/
-weather_lang = 'en' # see https://darksky.net/dev/docs/forecast for full list of language parameters values
-weather_unit = 'us' # see https://darksky.net/dev/docs/forecast for full list of unit parameters values
-latitude = None # Set this if IP location lookup does not work for you (must be a string)
-longitude = None # Set this if IP location lookup does not work for you (must be a string)
+time_format = 24 # 12 or 24
+date_format = "%d %B %Y" # check python doc for strftime() for options
+news_country_code = 'no'
+weather_api_token = '513ff3627ffaafcd4e48d46d916f3da0' # create account at https://darksky.net/dev/
+weather_lang = 'no' # see https://darksky.net/dev/docs/forecast for full list of language parameters values
+weather_unit = 'si' # see https://darksky.net/dev/docs/forecast for full list of unit parameters values
+latitude = '59.9139' # Set this if IP location lookup does not work for you (must be a string)
+longitude = '10.7522' # Set this if IP location lookup does not work for you (must be a string)
 xlarge_text_size = 94
 large_text_size = 48
-medium_text_size = 28
-small_text_size = 18
+medium_text_size = 32
+small_text_size = 22
 
 @contextmanager
 def setlocale(name): #thread proof function to work with locale
@@ -208,11 +208,11 @@ class Weather(Frame):
         return 1.8 * (kelvin_temp - 273) + 32
 
 
-class News(Frame):
+class Nyheter(Frame):
     def __init__(self, parent, *args, **kwargs):
         Frame.__init__(self, parent, *args, **kwargs)
         self.config(bg='black')
-        self.title = 'News' # 'News' is more internationally generic
+        self.title = 'Nyheter' # 'News' is more internationally generic
         self.newsLbl = Label(self, text=self.title, font=('Helvetica', medium_text_size), fg="white", bg="black")
         self.newsLbl.pack(side=TOP, anchor=W)
         self.headlinesContainer = Frame(self, bg="black")
@@ -225,10 +225,10 @@ class News(Frame):
             for widget in self.headlinesContainer.winfo_children():
                 widget.destroy()
             if news_country_code == None:
-                headlines_url = "http://www.nrk.no/nyheter/siste.rss"
+                headlines_url = "https://www.nrk.no/nyheter/siste.rss"
             else:
-                 headlines_url = "http://www.nrk.no/nyheter/siste.rss"               
-               # headlines_url = "https://news.google.com/news?ned=%s&output=rss" % news_country_code
+       		headlines_url = "https://www.nrk.no/nyheter/siste.rss"
+                # headlines_url = "https://news.google.com/news?ned=%s&output=rss" % news_country_code
 
             feed = feedparser.parse(headlines_url)
 
@@ -310,7 +310,7 @@ class FullscreenWindow:
         self.weather = Weather(self.topFrame)
         self.weather.pack(side=LEFT, anchor=N, padx=100, pady=60)
         # news
-        self.news = News(self.bottomFrame)
+        self.news = Nyheter(self.bottomFrame)
         self.news.pack(side=LEFT, anchor=S, padx=100, pady=60)
         # calender - removing for now
         # self.calender = Calendar(self.bottomFrame)
